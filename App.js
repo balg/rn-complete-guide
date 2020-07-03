25fd243fe10ee149b3331136ddb425c64fbca857
balg <2700278+balg@users.noreply.github.com>
2020-07-03 18:53:21 +0200

    Finishing game logic

--- App.js
+++ App.js
@@ -4,17 +4,38 @@ import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [rounds, setRounds] = useState(0);
 
   const startGameHandler = (number) => {
-    setUserNumber(number)
-  }
+    setUserNumber(number);
+  };
 
-  let content = <StartGameScreen onStartGame={startGameHandler} />
-  if (userNumber) {
-    content = <GameScreen userChoise={userNumber} />
+  const restartGameHandler = () => {
+    setUserNumber();
+    setRounds(0);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  if (userNumber && !rounds) {
+    content = (
+      <GameScreen userChoise={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (rounds) {
+    content = (
+      <GameOverScreen
+        rounds={rounds}
+        userNumber={userNumber}
+        onRestartGame={restartGameHandler}
+      />
+    );
   }
 
   return (
